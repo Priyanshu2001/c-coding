f8b7c7e2a84f2ffbaae067d5bb29d1119af89640
Priyanshu2001 <prm.bansal9@gmail.com>
2020-01-21 22:46:36 +0530

    another way to operate aithmetic operations

---
+++ Ao2.c
@@ -0,0 +1,13 @@
+//To add,subtract,multiply or divide two numbers
+#include<stdio.h>
+void main()
+{
+ float a,b;                            //these variables can alsi be taken into int except for division
+ printf("Enter the two numbers:- ");
+ scanf("%f%f",&a,&b);                            // %f is used for float type
+
+printf("The addition of given two numbers is :- %f",a+b);               //To add two numbers   
+printf("\nThe substraction of given two numbers is :- %f",a-b);        //To substract two numbers
+printf("\nThe multiplication of given two numbers is :- %f",a*b);     //To multiply two numbers    
+printf("\nThe division of given two numbers is :- %f",a/b);          //To divide two numbers
+}

0e3cd1c0ae639c8d5d7d775af540bae9ad0e4567
Priyanshu2001 <prm.bansal9@gmail.com>
2020-02-22 20:47:15 +0530

    to check wheter no. is even or odd

---
+++ Prog(1).c
@@ -0,0 +1,15 @@
+/* Author:- Priyanshu Bansal
+Github Username :- Priyanshu2001 */
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter the number:- ");
+    scanf("%d",&num);
+
+    if(num%2==0)
+    printf("Number is even"); //for only one statement there is no need to use brackets
+
+    else
+    printf("Number is odd");
+}

0135e9c1e4c45c8a26fe2d0d5ac29f13e4b66a6a
Priyanshu2001 <prm.bansal9@gmail.com>
2020-02-22 15:18:23 +0530

    to find remainder

---
+++ Ao3.c
@@ -0,0 +1,12 @@
+/* Author:- Priyanshu Bansal
+Github Username :- Priyanshu2001 */
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the two numbers:- ");
+    scanf("%d%d",&a,&b); 
+    c=a%b;
+    printf("the remainder is :- %d",c);
+}
+
